ac745e002ccccc549c883b19361e4a72abcf9837
Marcelo-Diament <marcelodiament@gmail.com>
2021-07-03 19:18:39 -0300

    feature: counter main script

---
+++ index.js
@@ -0,0 +1,12 @@
+import { consoleMsg } from './helpers/index.js'
+import counter from './inc/counter.js'
+
+const init = () => {
+  consoleMsg('Visite https://github.com/Marcelo-Diament/js-countdown-template', 'info')
+  counter()
+}
+
+window.onload = () => {
+  init()
+  consoleMsg('Obrigado por usar o js-countdown.', 'success')
+}
